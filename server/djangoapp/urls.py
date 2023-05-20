03505c3c019cd8070c4936a38582aefd4b00ad36
nuysanbar <nuysanbarorodev@gmail.com>
2023-05-19 22:24:01 -0400

    added some static files

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,15 +10,15 @@ urlpatterns = [
     # name the URL
 
     # path for about view
-
+    path(route="about",view=views.about, name="about"),
     # path for contact us view
-
+    path(route="contact",view=views.contact, name="contact"),
     # path for registration
 
     # path for login
 
     # path for logout
-
+    path(route='statics', view=views.static_files, name='static-files'),
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
